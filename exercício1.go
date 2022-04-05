db1711cbd0dfa0f6bddf88748bd34ae8d4c68312
Dryele coelho <90875948+dryelecoelho@users.noreply.github.com>
2022-04-05 14:34:02 -0400

    Create exercício1.go

---
+++ exercício1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 42 // operador curto
+	y := "james bond" // operador curto
+	z := true // operador curto
+
+	fmt.Println(x, y, z) // multiplas declaracoes
+	fmt.Println(x) // unica declaracao
+	fmt.Println(y) // unica declaracao
+	fmt.Println(z) // unica declaracao
+}
