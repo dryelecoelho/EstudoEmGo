2d301760ce87473327754a2ccea0e264a916bebf
Dryele coelho <90875948+dryelecoelho@users.noreply.github.com>
2022-04-06 11:00:53 -0400

    Create exercício2.go

---
+++ exercício2.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Variaçoes da declarações das variáveis
+
+var x int // indentificador é tipo int
+var y string // indentificador é tipo string
+var z bool // indentificador é tipo boo;
+
+func main() {
+	fmt.Printf("%v\n%v\n%v\n", x, y, z)
+}
